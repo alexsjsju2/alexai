853b4fe4f7d9216dbf8104a153e5f1c93ac37009
alexsjsju2 <alexsjsju0@gmail.com>
2025-11-21 14:55:51 +0100

    Update universal.js

--- api/universal.js
+++ api/universal.js
@@ -25,7 +25,7 @@ Rispondi brevemente, poeticamente.
 `;
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');  
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.alexsjsju.eu');  
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
